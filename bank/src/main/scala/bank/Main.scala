bd8bd5dc7f2c4c3bdb26690d0ac03c12bf709b56
agAlexandre <47420037+agAlexandre@users.noreply.github.com>
2020-06-02 16:44:38 -0300

    Update Main.scala

--- bank/src/main/scala/bank/Main.scala
+++ bank/src/main/scala/bank/Main.scala
@@ -3,7 +3,7 @@ package bank
 object Main {
   def main(args: Array[String]): Unit = {
     var account = new accountBank
-    account.openAccount("Alexandre",123,"ca")
+    account.openAccount("Name",123,"ca")
     account.accountStatement()
   }
 }
